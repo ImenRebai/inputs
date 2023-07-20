5b5f7769f424580f7e0c342adedaacbf06d1e984
Imen rebai <imenrb.info@gmail.com>
2023-07-20 19:20:48 +0100

    handleInputChange

---
+++ src/Componenets/MyInputs.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { useState } from "react"
+ const MyInputs = () => {
+    const [myInputValue, setMyInputValue]= useState("")
+    function handleInputChange(event){
+       setMyInputValue(event.target.value)
+    }
+  return (
+    <div>
+      <label > Your Name </label>
+      <input value = {myInputValue} onChange={handleInputChange}/> 
+    </div>
+  )
+}
+export default MyInputs
+
